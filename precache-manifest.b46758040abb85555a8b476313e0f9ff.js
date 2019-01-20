c006d13db6c90ca737ac83a1501d2d45b15dd32f
farha <farhajw@gmail.com>
2019-01-19 16:03:03 -0800

    Updates

---
+++ precache-manifest.b46758040abb85555a8b476313e0f9ff.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/inspiration-portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "6e8334b88457390a31a6",
+    "url": "/inspiration-portfolio/static/css/main.827236bf.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/inspiration-portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "583620e9f868db10574b",
+    "url": "/inspiration-portfolio/static/js/1.583620e9.chunk.js"
+  },
+  {
+    "revision": "2f055fd5e2d1b05b5529",
+    "url": "/inspiration-portfolio/static/js/runtime~main.2f055fd5.js"
+  },
+  {
+    "revision": "6e8334b88457390a31a6",
+    "url": "/inspiration-portfolio/static/js/main.6e8334b8.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/inspiration-portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/inspiration-portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/inspiration-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "583620e9f868db10574b",
+    "url": "/inspiration-portfolio/static/css/1.5381f0c8.chunk.css"
+  },
+  {
+    "revision": "800041e87bfd7de7ce50f18455772001",
+    "url": "/inspiration-portfolio/index.html"
+  }
+];
